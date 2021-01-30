72f528393d843c081a677ce351a6b204173a014a
MCAlberto55 <alberto99emc@outlook.com>
2021-01-30 12:08:21 -0600

    create a protected list to validate destiny

--- src/Command.java
+++ src/Command.java
@@ -1,5 +1,17 @@
 package src;
 
+import java.util.ArrayList;
+
 public abstract class Command {
+
+    protected ArrayList<String> directions = new ArrayList<String>();
+
+    public Command() {
+        directions.add("north");
+        directions.add("east");
+        directions.add("south");
+        directions.add("west");
+    }
+
     public abstract void run(GameState currentState);
 }
