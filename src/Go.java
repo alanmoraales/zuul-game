0b476cf14b7d48da69ab24d3a3ba3993ee5cfc7d
MCAlberto55 <alberto99emc@outlook.com>
2021-01-30 12:09:22 -0600

    implement Go class funcionality

--- src/Go.java
+++ src/Go.java
@@ -2,8 +2,34 @@ package src;
 
 public class Go extends Command{
 
+	private String destiny;
+
 	@Override
 	public void run(GameState currentState) {
 
+		if(this.directions.contains(destiny)) {
+			if(destiny == "north") {
+				Room newRoom = currentState.getCurrentRoom().getNorthExit();
+				currentState.setCurrentRoom(newRoom);
+			}
+			if(destiny == "east") {
+				Room newRoom = currentState.getCurrentRoom().getEastExit();
+				currentState.setCurrentRoom(newRoom);
+			}
+			if(destiny == "south") {
+				Room newRoom = currentState.getCurrentRoom().getSouthExit();
+				currentState.setCurrentRoom(newRoom);
+			}
+			if(destiny == "west") {
+				Room newRoom = currentState.getCurrentRoom().getWestExit();
+				currentState.setCurrentRoom(newRoom);
+			}
+		}else {
+			System.out.println("No existe esa direccion");
+		}
+	}
+
+	public void setDestiny(String destiny) {
+		this.destiny = destiny;
 	}
 }
