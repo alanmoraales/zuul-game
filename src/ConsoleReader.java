4c692803f97b07cf76b8a29ec671a46f15dc9204
Alan Morales <alanmoraales845@gmail.com>
2021-02-08 20:55:55 -0600

    fix: closing reader causes errors

--- src/ConsoleReader.java
+++ src/ConsoleReader.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class ConsoleReader {
   public String readCommand() {
     var commandReader = new Scanner(System.in);
-    var command = commandReader.nextLine();
-    commandReader.close();
-    return command;
+    return commandReader.nextLine();
   }
 }
